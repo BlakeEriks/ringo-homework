fcde5648e2f005aa9f4c552bc9c29e2f577b6542
Blake Eriks <eblake98@vt.edu>
2021-09-17 19:24:06 -1000

    added ejs files for food and centralized styles

--- unit-2/gitpub/server.js
+++ unit-2/gitpub/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const drinks = require('./models/drinks');
+const food = require('./models/food')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
 });
+
+app.get('/food', (req,res) => {
+    res.render('food_index.ejs', {food : food})
+})
 
 app.get('/drinks', (req, res) => {
     res.render('drinks_index.ejs', {drinks : drinks});
@@ -14,6 +21,6 @@ app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', {drink : drinks[req.params.id]});
 });
 
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log('listening on port 3000');
 });
