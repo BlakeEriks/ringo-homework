c8d38a07f9a83d09f6bc0d70b8da2914455a5168
Blake Eriks <eblake98@vt.edu>
2021-09-18 10:31:55 -1000

    added index route

--- unit-2/uncharted-planet/server.js
+++ unit-2/uncharted-planet/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
+const scientists = []
+
+/* MIDDLEWARE */
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
-    res.render('new.ejs')
+    res.render('index.ejs', {scientists: scientists})
+})
+
+app.post('/', (req,res) => {
+    scientists.push(req.body)
+    res.redirect('/')
 })
 
 app.get('/new', (req,res) => {
     res.render('new.ejs')
+})
+
+app.get('/:index', (req, res) => {
+    res.render('show.ejs', {scientist: scientists[req.params.index]})
 })
 
 app.listen(3000)
