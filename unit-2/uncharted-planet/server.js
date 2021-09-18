ff82d95c9b6e43a355f31294860558d4e4d8422b
Blake Eriks <eblake98@vt.edu>
2021-09-18 10:44:42 -1000

    finishing up

--- unit-2/uncharted-planet/server.js
+++ unit-2/uncharted-planet/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 const scientists = []
 
 /* MIDDLEWARE */
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req,res) => {
     res.render('index.ejs', {scientists: scientists})
@@ -16,6 +18,11 @@ app.post('/', (req,res) => {
 
 app.get('/new', (req,res) => {
     res.render('new.ejs')
+})
+
+app.delete('/:index', (req,res) => {
+    scientists.splice(req.params.index, 1);
+    res.redirect('/')
 })
 
 app.get('/:index', (req, res) => {
