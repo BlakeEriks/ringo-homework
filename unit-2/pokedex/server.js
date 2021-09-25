46926cdb4ba69f346c7545dc331e56948808efd9
Blake Eriks <eblake98@vt.edu>
2021-09-25 09:11:46 -1000

    adding new page and route

--- unit-2/pokedex/server.js
+++ unit-2/pokedex/server.js
@@ -19,11 +19,25 @@ app.use(express.static('public'))
 //  ROUTES
 /////////////////////////////
 app.get('/pokemon', (req,res) => {
+    if (req.query.id) res.redirect('/pokemon/' + req.query.id);
     res.render('index.ejs', {pokemon: pokemon})
 })
 
 app.post('/pokemon', (req,res) => {
+    req.body.stats = {  hp: req.body.hp,
+                        attack: req.body.attack,
+                        defense: req.body.defence,
+                        spattack: req.body.spattack,
+                        spdefence: req.body.spdefence,
+                        speed: req.body.speed}
+    req.body.type = req.body.type.split(',')
+    console.log(req.body)
     pokemon.push(req.body)
+    res.redirect('/pokemon')
+})
+
+app.put('/pokemon/:id', (req,res) => {
+    pokemon[req.params.id-1] = req.body;
     res.redirect('/pokemon')
 })
 
@@ -31,11 +45,16 @@ app.get('/pokemon/new', (req,res) => {
     res.render('new.ejs')
 })
 
+app.delete('/pokemon/:id', (req,res) => {
+    pokemon.splice(req.params.id-1,1);
+    res.redirect('/pokemon')
+})
+
 app.get('/pokemon/:id/edit', (req,res) => {
     res.render('edit.ejs')
 })
 
-app.get('pokemon/:id', (req,res) => {
+app.get('/pokemon/:id', (req,res) => {
     res.render('show.ejs', {pokemon: pokemon[req.params.id-1], typeColors: typeColors})
 })
 
