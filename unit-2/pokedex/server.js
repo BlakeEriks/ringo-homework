9bdb7ce0f53a3c98ef6fa936fc68b7deb8ffbd20
Blake Eriks <eblake98@vt.edu>
2021-09-25 06:29:44 -1000

    created show page and more routes

--- unit-2/pokedex/server.js
+++ unit-2/pokedex/server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 const pokemon = require('./models/pokemon')
+var typeColors = require('./public/typeColors.json')
 
 /////////////////////////////
 //  MIDDLEWARE
@@ -17,8 +18,25 @@ app.use(express.static('public'))
 /////////////////////////////
 //  ROUTES
 /////////////////////////////
-app.get('/', (req,res) => {
+app.get('/pokemon', (req,res) => {
     res.render('index.ejs', {pokemon: pokemon})
+})
+
+app.post('/pokemon', (req,res) => {
+    pokemon.push(req.body)
+    res.redirect('/pokemon')
+})
+
+app.get('/pokemon/new', (req,res) => {
+    res.render('new.ejs')
+})
+
+app.get('/pokemon/:id/edit', (req,res) => {
+    res.render('edit.ejs')
+})
+
+app.get('pokemon/:id', (req,res) => {
+    res.render('show.ejs', {pokemon: pokemon[req.params.id-1], typeColors: typeColors})
 })
 
 app.listen(3000);
