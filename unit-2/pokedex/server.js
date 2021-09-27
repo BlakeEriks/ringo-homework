b2c4c0d516d1b3843acfadb9b150d5568333215b
Blake Eriks <eblake98@vt.edu>
2021-09-26 16:42:08 -1000

    fedit route and styling changes

--- unit-2/pokedex/server.js
+++ unit-2/pokedex/server.js
@@ -18,26 +18,41 @@ app.use(express.static('public'))
 /////////////////////////////
 //  ROUTES
 /////////////////////////////
+app.get('/', (req,res) => {
+    res.redirect('/pokemon')
+})
+
 app.get('/pokemon', (req,res) => {
     if (req.query.id) res.redirect('/pokemon/' + req.query.id);
     res.render('index.ejs', {pokemon: pokemon})
 })
 
 app.post('/pokemon', (req,res) => {
+    if (pokemon[req.body.id]) res.redirect('/pokemon/error')
     req.body.stats = {  hp: req.body.hp,
                         attack: req.body.attack,
                         defense: req.body.defence,
                         spattack: req.body.spattack,
                         spdefence: req.body.spdefence,
-                        speed: req.body.speed}
+                        speed: req.body.speed
+                    }
     req.body.type = req.body.type.split(',')
-    console.log(req.body)
     pokemon.push(req.body)
     res.redirect('/pokemon')
 })
 
 app.put('/pokemon/:id', (req,res) => {
-    pokemon[req.params.id-1] = req.body;
+    req.body.stats = {  hp: req.body.hp,
+                        attack: req.body.attack,
+                        defense: req.body.defense,
+                        spattack: req.body.spattack,
+                        spdefense: req.body.spdefense,
+                        speed: req.body.speed
+                    }
+    req.body.type = req.body.type.split(',')
+    let pokeIndex = pokemon.findIndex( poke => poke.id === req.params.id )
+    pokemon[pokeIndex] = req.body;
+    console.log(pokemon[pokeIndex])
     res.redirect('/pokemon')
 })
 
@@ -46,16 +61,28 @@ app.get('/pokemon/new', (req,res) => {
 })
 
 app.delete('/pokemon/:id', (req,res) => {
-    pokemon.splice(req.params.id-1,1);
+    let pokeIndex = pokemon.findIndex( poke => poke.id === req.params.id )
+    pokemon.splice(pokeIndex,1);
     res.redirect('/pokemon')
 })
 
 app.get('/pokemon/:id/edit', (req,res) => {
-    res.render('edit.ejs')
+    console.log(req.params.id)
+    res.render('edit.ejs', {pokemon: pokemon.find( poke => poke.id === req.params.id)})
+})
+
+app.get('/pokemon/error', (req,res) => {
+    res.render('error.ejs')
 })
 
 app.get('/pokemon/:id', (req,res) => {
-    res.render('show.ejs', {pokemon: pokemon[req.params.id-1], typeColors: typeColors})
+    let poke = pokemon.find( item => Number(item.id) === Number(req.params.id));
+    if (poke) {
+        res.render('show.ejs', {pokemon: poke, typeColors: typeColors})
+    }
+    else {
+        res.redirect('/pokemon/error')
+    }
 })
 
 app.listen(3000);
