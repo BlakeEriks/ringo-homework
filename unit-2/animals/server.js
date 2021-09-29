2461326c178856f8f6565a91aac0df5c529bf413
Blake Eriks <eblake98@vt.edu>
2021-09-29 13:22:17 -1000

    setting up server.js for animals lab

---
+++ unit-2/animals/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const AnimalRouter = require('./controllers/animal')
+
+/* Create */
+const app = express()
+
+/* Middleware */
+app.use(morgan("tiny"))                 // Logging
+app.use(methodOverride('_method'))      // Override for put and delete requests
+app.use(express.urlencoded({extended: false}))   // parse urlencoded request
+app.use(express.static('public'))       //serve files from public statically
+app.use('/animals', AnimalRouter)
+
+
+/* Server Listener */
+app.get('/', (req,res) => {
+    res.send('server is going boys')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log('Listening on port' + PORT))
