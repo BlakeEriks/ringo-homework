4ae0dd29cdf2a9b4918d7f595c34c793eccfa08b
Blake Eriks <eblake98@vt.edu>
2021-10-02 09:28:26 -1000

    moved middleware to its own utility file

--- unit-2/animals/server.js
+++ unit-2/animals/server.js
@@ -1,33 +1,15 @@
-require("dotenv").config(); 
 const express = require('express')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const AnimalRouter = require('./controllers/animal')
-const UserRouter = require('./controllers/user')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
+const middleware = require('./utils/middleware')
 
 /* Create */
 const app = express()
 
 /* Middleware */
-app.use(morgan("tiny"))                 // Logging
-app.use(methodOverride('_method'))      // Override for put and delete requests
-app.use(express.urlencoded({extended: false}))   // parse urlencoded request
-app.use(express.static('public'))       //serve files from public statically
-app.use(session({
-    secret: process.env.SECRET,
-    store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
-    saveUninitialized: true,
-    resave: false,
-}))
-app.use('/animals', AnimalRouter)
-app.use('/user', UserRouter)
+middleware(app)
 
 /* Server Listener */
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log('Listening on port ' + PORT))
+app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT))
