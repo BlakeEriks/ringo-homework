19351d4967e7438726fd1c36d5c53ef5a6301de3
Blake Eriks <eblake98@vt.edu>
2021-10-02 05:45:33 -1000

    Adding header with username + log in / out buttons

--- unit-2/animals/controllers/animal.js
+++ unit-2/animals/controllers/animal.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.use((req,res,next) => {
     if (req.session.loggedIn) {
+        res.locals.username = req.session.username
         next();
     }
     else {
