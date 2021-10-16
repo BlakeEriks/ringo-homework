850e261199e747998d3c1c91616326d3fd95caf5
Blake Eriks <eblake98@vt.edu>
2021-10-16 09:29:06 -1000

    Create navbar component

---
+++ unit-3/dashboard/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='navbar-element selected'>Dashboard</div>
+            <div className='navbar-element'>Widget</div>
+            <div className='navbar-element'>Reviews</div>
+            <div className='navbar-element'>Customers</div>
+            <div className='navbar-element'>Online Analysis</div>
+            <div className='navbar-element'>Settings</div>
+        </div>
+    )
+}
+
+export default Navbar
