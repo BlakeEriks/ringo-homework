06de30701b2eeb9c895ac9f23af57db12f9630b7
Blake Eriks <eblake98@vt.edu>
2021-10-16 10:31:43 -1000

    Adding content to large card

--- unit-3/dashboard/src/Components/Dashboard.js
+++ unit-3/dashboard/src/Components/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
                 <Card className='small' title='Average Rating' content='4.5/5'/>
                 <Card className='small' title='Sentiment Analysis' content='Love the view'/>
             </div>
-            <Card className='large' title='Website Visitors'/>
+            <Card className='large' title='Website Visitors' content="Beautiful graph with beautiful data"/>
         </div>
     )
 }
