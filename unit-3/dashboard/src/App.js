ed14200ef656790605b48c95bec12d5879de937a
Blake Eriks <eblake98@vt.edu>
2021-10-16 09:27:47 -1000

    Create dashboard lab

---
+++ unit-3/dashboard/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar";
+import Dashboard from "./Components/Dashboard";
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Dashboard />
+    </main>
+  );
+}
+
+export default App;
