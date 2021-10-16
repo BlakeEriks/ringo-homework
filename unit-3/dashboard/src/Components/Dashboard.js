60e0006b90524ee99771dd36d98f2effc71fa889
Blake Eriks <eblake98@vt.edu>
2021-10-16 09:28:37 -1000

    Create dashboard component

---
+++ unit-3/dashboard/src/Components/Dashboard.js
@@ -0,0 +1,16 @@
+import Card from './Card'
+
+const Dashboard = () => {
+    return (
+        <div className='dashboard'>
+            <div className='top-row'>
+                <Card className='small' title='Reviews' content='Great prices'/>
+                <Card className='small' title='Average Rating' content='4.5/5'/>
+                <Card className='small' title='Sentiment Analysis' content='Love the view'/>
+            </div>
+            <Card className='large' title='Website Visitors'/>
+        </div>
+    )
+}
+
+export default Dashboard
