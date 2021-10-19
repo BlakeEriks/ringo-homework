7236798393d5486eb75d9ff74e9b4d82ab8ba056
Blake Eriks <eblake98@vt.edu>
2021-10-19 13:50:23 -1000

    Create weatherData component

---
+++ unit-3/react-weather-forecast/src/Components/WeatherForecast.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WeatherForecast = ({img, conditions, time}) => {
+
+    return (
+        <div className="weather">
+            <img src={img} alt="" />
+            <p><span>conditions:</span> {conditions}</p>
+            <p><span>time:</span> {time}</p>
+        </div>
+    );
+};
+
+export default WeatherForecast
