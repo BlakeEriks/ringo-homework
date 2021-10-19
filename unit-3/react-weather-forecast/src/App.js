2d7fbc760e9c3f58ae21b92acd9d3d2a39a538d3
Blake Eriks <eblake98@vt.edu>
2021-10-19 13:51:28 -1000

    Adding weatherForecast components and import weather data

--- unit-3/react-weather-forecast/src/App.js
+++ unit-3/react-weather-forecast/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./styles.css";
+import data from "./weatherData"
+import WeatherForecast from "./Components/WeatherForecast"
 
 export default function App() {
   return (
     <div className="App">
       <h1>My WeatherIcons App</h1> 
+      <section>
+        {data.map( (forecast, index) => <WeatherForecast {...forecast} key={index} />)}
+      </section>
     </div>
   );
 }
