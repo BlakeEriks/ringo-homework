112ccef047841710882507f06c6987a22c96b7da
Blake Eriks <eblake98@vt.edu>
2021-10-27 15:35:49 -1000

    Style Nav

--- unit-3/react-router/src/components/Nav.js
+++ unit-3/react-router/src/components/Nav.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 const Nav = () => {
     return (
         <div className="nav">
-            <Link to="/">
+            <Link to="/" className="nav-item">
                 <div>Home</div>
             </Link>
-            <Link to="/about">
+            <Link to="/about" className="nav-item">
                 <div>About</div>
             </Link>
-            <Link to="/stocks">
+            <Link to="/stocks" className="nav-item">
                 <div>Stocks</div>
             </Link>
         </div>
