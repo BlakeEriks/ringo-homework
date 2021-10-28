1e40f22a607a055f7b6cecc2d74d22311b6436b3
Blake Eriks <eblake98@vt.edu>
2021-10-27 15:36:24 -1000

    Create Home Component content

--- unit-3/react-router/src/pages/Home.js
+++ unit-3/react-router/src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+    <>
+        <h1>Welcome to my React Router Lab!</h1>
+        <h2>Have a look around, don't be shy ;)</h2>
+    </>
+    )
 }
 
 export default Home
