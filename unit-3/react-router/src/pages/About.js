e3cc5183e74e190bd09fc0051c4d03addd23505d
Blake Eriks <eblake98@vt.edu>
2021-10-27 15:35:57 -1000

    Write an about section

--- unit-3/react-router/src/pages/About.js
+++ unit-3/react-router/src/pages/About.js
@@ -1,5 +1,14 @@
 const About = () => {
-    return <h1>About</h1>
+    return (
+    <>
+    <h1>This app was created to demonstrate</h1>
+    <h1>my abilitity to create a React app</h1>
+    <h1>which uses the Switch and Routing features</h1>
+    <h1>available from the react-router and</h1>
+    <h1>react-router-dom packages.</h1>
+    <h1>Have a blessed day {'<3'}</h1>
+    </>
+    )
 }
 
 export default About
