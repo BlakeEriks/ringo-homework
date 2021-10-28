97e1e22c96ead000b3877bc3a85ee2529ee9aa41
Blake Eriks <eblake98@vt.edu>
2021-10-27 15:36:09 -1000

    Create ticker dashboard

---
+++ unit-3/react-router/src/pages/Dashboard.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+import tickerData from "../tickerData"
+
+const Dashboard = props => {
+
+    const tickers = tickerData.map( (ticker,index) => {
+
+        // let change = 
+
+        return (
+            <Link to={`/stocks/${ticker.symbol}`} key={index}> 
+                <h2>
+                    {ticker.name} 
+                    <span className={ticker.change > 0 ? 'green' : 'red'}>
+                    {' (' + ticker.symbol + ' '} 
+                    {ticker.change > 0 ? '+' : ''}{ticker.change.toPrecision(2) + '%'}
+                    </span>
+                    {')'}
+                </h2>
+            </Link>
+        )
+    }
+    )
+    
+    return (
+        <div className="tickers">
+            {tickers}
+        </div>
+    )
+}
+
+export default Dashboard
